4b3c4978f4b040150ef9b5cbeebc51a03961ea7f
Ryan Carniato <ryansolid@gmail.com>
2019-07-20 00:44:21 -0700

    update to solid 0.9.0

--- rollup.config.js
+++ rollup.config.js
@@ -14,7 +14,7 @@ export default {
 	plugins: [
 		babel({
       exclude: 'node_modules/**',
-      plugins: [["jsx-dom-expressions", {moduleName: 'solid-js/dom'}]]
+      presets: ['solid']
     }),
     resolve({ extensions: ['.js', '.jsx'] }),
 		production && terser()
